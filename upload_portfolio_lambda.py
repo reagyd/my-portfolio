4848d130eba530b4e2d5e6f28ba82256859c8fb5
Reagan Diaz <reagan.diaz@gmail.com>
2017-09-16 17:38:16 +1200

    creation of lambda script

--- upload_portfolio_lambda.py
+++ upload_portfolio_lambda.py
@@ -13,7 +13,7 @@ portfolio_zip = StringIO.StringIO()
 build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
 with zipfile.ZipFile(portfolio_zip) as myzip:
-    for rm in myzip.namelist():
+    for nm in myzip.namelist():
         obj = myzip.open(nm)
         portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
