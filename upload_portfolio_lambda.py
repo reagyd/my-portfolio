699df00ee225286abb28a49275091e6e2472ef5b
Reagan Diaz <reagan.diaz@gmail.com>
2017-09-16 17:20:46 +1200

    creation of lambda script

---
+++ upload_portfolio_lambda.py
@@ -0,0 +1,20 @@
+import boto3
+from botocore.Client import Config
+import String IO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+portfolio_bucket = s3.Bucket('portfolio.mybackendserver.com')
+build_bucket = s3.Bucket('portfoliobuilds.mybackendserver.com')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for rm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+                
